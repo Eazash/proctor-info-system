0a5775297c71f2120687955337e57c0055f4127b
Eazash <56728970+Eazash@users.noreply.github.com>
2019-12-27 23:09:07 +0300

    Class Employee Defined

---
+++ src/Entities/Employee.java
@@ -0,0 +1,22 @@
+package Entities;
+
+public class Employee extends Person {
+    private double salary;
+    private String employeeId;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
